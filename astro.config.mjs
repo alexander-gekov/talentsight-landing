b0d9195896977401961239fa9ec4e4b4666352e9
Aleksandar Gekov <alexander.gekov00@gmail.com>
2023-07-12 21:46:34 +0200

    fix astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import basicSsl from '@vitejs/plugin-basic-ssl'
-const env = loadEnv("", process.cwd(), 'STORYBLOK')
-
+import basicSsl from "@vitejs/plugin-basic-ssl";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
   site: "https://talentsight.netlify.app",
@@ -21,18 +20,19 @@ export default defineConfig({
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
       components: {
-        page: 'storyblok/Page',
-        feature: 'storyblok/Feature',
-        grid: 'storyblok/Grid',
-        icon: 'storyblok/Icon',
-        hero: 'storyblok/Hero',
-        features: 'storyblok/Features',
-        logos: 'storyblok/Logos',
-        cta: 'storyblok/Cta',
-        section_head: 'storyblok/SectionHead',
-        heading: 'storyblok/Heading',
-        gallery: 'storyblok/Gallery',
-        member: 'storyblok/Member'
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        icon: "storyblok/Icon",
+        hero: "storyblok/Hero",
+        features: "storyblok/Features",
+        logos: "storyblok/Logos",
+        cta: "storyblok/Cta",
+        section_head: "storyblok/SectionHead",
+        heading: "storyblok/Heading",
+        gallery: "storyblok/Gallery",
+        member: "storyblok/Member",
+        howto: "storyblok/Howto",
       },
     }),
   ],
